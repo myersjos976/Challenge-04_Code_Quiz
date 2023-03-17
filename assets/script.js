8cb6b1e2f8faa1efd833703059c42b67ebfedbb9
Joshua Myers <jt.myers976@gmail.com>
2023-03-16 22:36:42 -0600

    Fixed answer buttons and isCorrect message. Also made some formatting changes to code.

--- assets/script.js
+++ assets/script.js
@@ -11,16 +11,19 @@ var question9 = document.querySelector("#question-9");
 var question10 = document.querySelector("#question-10");
 
 var startButton = document.querySelector("#start-button");
-var aButton = document.querySelector("answer-a");
-var bButton = document.querySelector("answer-b");
-var cButton = document.querySelector("answer-c");
-var dButton = document.querySelector("answer-d");
-
 var timerEl = document.querySelector("#timer");
+var isCorrectEl = document.getElementById("bottom-bar");
+
+var timeLeft;
+var currentQuestion;
 var answerKey = ['b', 'd', 'a', 'c', 'b', 'c', 'd', 'c', 'b', 'a'];
 var questions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10];
 
-// Make start page visible and hide all others.
+
+/* 
+    Makes start page visible and hides all others.
+    Start page contains a start quiz button which when pressed will call startQuiz().
+*/
 function openStartPage()
 {
     startPage.style.display = 'block';
@@ -38,55 +41,139 @@ function startQuiz()
 
 // Creates a 5 minute timer.
 function startTimer() {
-    var timeLeft = 400;
+    timeLeft = 400;
     var timeInterval = setInterval(function () {
-        if (timeLeft > 1)
-        {
+        if (timeLeft > 1) {
             timerEl.textContent = timeLeft;
-            console.log(timeLeft);
             timeLeft--;
         }
-        else 
-        {
+        else {
             timerEl.textContent = "";
             clearInterval(timeInterval);
         }
     }, 1000);
 }
 
-//Displays quiz question of number starting at 1.
+/*
+    Displays the question page corresponding to the input number.
+    Updates the currentQuestion and hides all sections except for the currentQuestion's page.
+*/
 function openQuestion(number)
 {
+    currentQuestion = number;
     startPage.style.display = 'none';
-    for(let x = 0; x < questions.length; x++)
+    for (let x = 0; x < questions.length; x++)
     {
-        questions[x].this.style.length = 'none';
+        questions[x].style.display = 'none';
     }
-    questions[number - 1].style.length = 'block';
+    questions[number - 1].style.display = 'block';
+
+    /*  
+    The 4 following event listeners create 4 buttons for each question.
+    If answer matches answerKey, than correct is displayed.
+    Else, the answer doesn't match answerKey, so incorrect is displayed and timer is decremented by 10.
+    After, the next question is opened.
+    */
+    var aButton = document.querySelector("#answer-a" + number);
+    var bButton = document.querySelector("#answer-b" + number);
+    var cButton = document.querySelector("#answer-c" + number);
+    var dButton = document.querySelector("#answer-d" + number);
+
+
+    aButton.addEventListener("click", function(event) {
+        console.log("Clicked");
+        if (answerKey[currentQuestion -  1] === 'a') {
+            isCorrectEl.innerText = "Correct";
+        }
+        else {
+            isCorrectEl.innerText = "Incorrect";
+            timeLeft = timeLeft - 10;
+        }
+
+        if (currentQuestion < 10) {
+            openQuestion(currentQuestion + 1);
+        }
+
+        else {
+            openQuizOver();
+        }
+    });
+
+    bButton.addEventListener("click", function(event) {
+        console.log("Clicked");
+        if (answerKey[currentQuestion - 1] === 'b') {
+            isCorrectEl.textContent = "Correct";
+        }
+        else {
+            isCorrectEl.textContent = "Incorrect";
+            timeLeft = timeLeft - 10;
+        }
+
+        if (currentQuestion < 10){
+            openQuestion(currentQuestion + 1);
+        }
+        else {
+            openQuizOver();
+        }
+    });
+
+    cButton.addEventListener("click", function(event) {
+        console.log("Clicked");
+        if (answerKey[currentQuestion - 1] === 'c') {
+            isCorrectEl.textContent = "Correct";
+        }
+        else {
+            isCorrectEl.textContent = "Incorrect";
+            timeLeft = timeLeft - 10;
+        }
+
+        if (currentQuestion < 10) {
+            openQuestion(currentQuestion + 1);
+        }
+        else {
+            openQuizOver();
+        }
+    });
+
+    dButton.addEventListener("click", function(event) {
+        console.log("Clicked");
+        if (answerKey[currentQuestion - 1] === 'd') {
+            isCorrectEl.textContent = "Correct";
+        }
+        else {
+            isCorrectEl.textContent = "Incorrect";
+            timeLeft = timeLeft - 10;
+        }
+
+        if (currentQuestion < 10) {
+            openQuestion(currentQuestion + 1);
+        }
+        else {
+            openQuizOver();
+        }
+    });
 }
 
-// When page is opened, load start page.
+/*
+    Called once the quiz has ended (whether by running out of time or all questions being answered).
+    The timer is cleared and the quizOver screen opened. This screen allows the user to see their score
+    and enter their initials into the high scores list. 
+*/
+function openQuizOver()
+{
+    clearInterval();
+
+    //TODO: Create input box for user's intitials for the high score list and save user's score information.
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// When webpage is opened, the start page is opened first.
 openStartPage();
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // When start button is pressed, start the quiz.
 startButton.addEventListener("click", function(event) {
     startQuiz();
 });
-
-// Creates buttons for each question.
-// aButton.addEventListener("click", function(event) {
-
-// });
-
-// bButton.addEventListener("click", function(event) {
-
-// })
-
-// cButton.addEventListener("click", function(event) {
-    
-// })
-
-// dButton.addEventListener("click", function(event) {
-    
-// })
